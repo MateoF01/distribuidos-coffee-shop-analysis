d563c6e1c13d3374c009612edba148fe18e428fb
Mateo Fernandez <mateofernandez380@gmail.com>
2025-12-05 18:05:50 -0300

    revive topper sin wsm dedicado. Conecado al coordinator

--- topper/wsm_config.py
+++ topper/wsm_config.py
@@ -1,17 +1,8 @@
 WSM_NODES = {
-    "topper_q2": [
-        ("wsm_topper_q2", 9661),
-        ("wsm_topper_q2_2", 9662),
-        ("wsm_topper_q2_3", 9663),
+    "coordinator": [
+        ("wsm_coordinators", 9901),
+        ("wsm_coordinators_2", 9902),
+        ("wsm_coordinators_3", 9903),
     ],
-    "topper_q3": [
-        ("wsm_topper_q3", 9671),
-        ("wsm_topper_q3_2", 9672),
-        ("wsm_topper_q3_3", 9673),
-    ],
-    "topper_q4": [
-        ("wsm_topper_q4", 9681),
-        ("wsm_topper_q4_2", 9682),
-        ("wsm_topper_q4_3", 9683),
-    ],
+
 }
